7afdc830f0bf2566b6f4433ebd7e8b6305dcaecc
김경환 <k2sc2000@gmail.com>
2025-03-13 12:12:35 +0900

    feat: 패키지 구조 설계

---
+++ backend/src/main/java/com/picscore/backend/common/model/response/BaseResponse.java
@@ -0,0 +1,39 @@
+package com.picscore.backend.common.model.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 기본적인 json 전송 방식 정의
+ */
+@Getter
+@Setter
+public class BaseResponse<T> {
+    private boolean success;
+    private String message;
+    private T data;
+    private String errorCode;
+
+    public BaseResponse(boolean success, String message, T data, String errorCode) {
+        this.success = success;
+        this.message = message;
+        this.data = data;
+        this.errorCode = errorCode;
+    }
+
+    public static <T> BaseResponse<T> success(T data) {
+        return new BaseResponse<>(true, "Success", data, null);
+    }
+
+    public static <T> BaseResponse<T> success(String message, T data) {
+        return new BaseResponse<>(true, message, data, null);
+    }
+
+    public static <T> BaseResponse<T> error(String message) {
+        return new BaseResponse<>(false, message, null, null);
+    }
+    public static <T> BaseResponse<T> error(String message, String errorCode) {
+        return new BaseResponse<>(false, message, null, errorCode);
+    }
+}
+
