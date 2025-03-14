bd619c3b8e0ce61fd6731a5180961cd27a1d882c
김경환 <k2sc2000@gmail.com>
2025-03-15 00:43:08 +0900

    feat: JWT Util 설정

--- backend/src/main/java/com/picscore/backend/photo/entity/Photo.java
+++ backend/src/main/java/com/picscore/backend/photo/entity/Photo.java
@@ -1,47 +1,47 @@
-package com.picscore.backend.photo.entity;
-
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.picscore.backend.user.model.entity.User;
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.data.annotation.CreatedDate;
-
-import java.time.LocalDateTime;
-
-@Entity
-@Table(name = "photo")
-@Getter @Setter
-@JsonInclude(JsonInclude.Include.ALWAYS) // null 가능, activity 통한 것은 null 가능
-public class Photo {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "photo_id")
-    @OneToMany(mappedBy = "photo")
-    @OneToOne(mappedBy = "photo")
-    private Long id;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
-
-    @Column(name = "image_url", nullable = false)
-    private String imageUrl;
-
-    @Column(name = "score", nullable = false)
-    private Float score;
-
-    @Column(name = "analysis_chart", columnDefinition = "JSON")
-    private String analysisChart;
-
-    @Column(name = "analysis_text", columnDefinition = "JSON")
-    private String analysisText;
-
-    @Column(name = "is_public", nullable = false)
-    private Boolean isPublic;
-
-    @CreatedDate
-    @Column(name = "created_at", nullable = false)
-    private LocalDateTime createdAt;
-
-}
+//package com.picscore.backend.photo.entity;
+//
+//import com.fasterxml.jackson.annotation.JsonInclude;
+//import com.picscore.backend.user.model.entity.User;
+//import jakarta.persistence.*;
+//import lombok.Getter;
+//import lombok.Setter;
+//import org.springframework.data.annotation.CreatedDate;
+//
+//import java.time.LocalDateTime;
+//
+//@Entity
+//@Table(name = "photo")
+//@Getter @Setter
+//@JsonInclude(JsonInclude.Include.ALWAYS) // null 가능, activity 통한 것은 null 가능
+//public class Photo {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "photo_id")
+//    @OneToMany(mappedBy = "photo")
+//    @OneToOne(mappedBy = "photo")
+//    private Long id;
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "user_id", nullable = false)
+//    private User user;
+//
+//    @Column(name = "image_url", nullable = false)
+//    private String imageUrl;
+//
+//    @Column(name = "score", nullable = false)
+//    private Float score;
+//
+//    @Column(name = "analysis_chart", columnDefinition = "JSON")
+//    private String analysisChart;
+//
+//    @Column(name = "analysis_text", columnDefinition = "JSON")
+//    private String analysisText;
+//
+//    @Column(name = "is_public", nullable = false)
+//    private Boolean isPublic;
+//
+//    @CreatedDate
+//    @Column(name = "created_at", nullable = false)
+//    private LocalDateTime createdAt;
+//
+//}
