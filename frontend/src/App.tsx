910bee6ab07df482458af4a1dd45df287b7751b6
kub938 <kub938@naver.com>
2025-03-14 22:36:11 +0900

    design: 백그라운드 컬러 변경

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,10 @@ import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <div className="flex flex-col max-w-md mx-auto min-h-screen">
+    <div
+      className="border flex flex-col max-w-md mx-auto min-h-screen bg-gray-50
+    "
+    >
       <header></header>
       <main>
         <Outlet />
