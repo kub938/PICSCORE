366779ee069bac96ec1812b3d62d07f0c24f66ae
xoghd32 <xoghd32@naver.com>
2025-03-18 13:46:23 +0900

    feat: 내사진 조회 ìAPIëAPI 완료

--- backend/src/main/java/com/picscore/backend/photo/controller/PhotoController.java
+++ backend/src/main/java/com/picscore/backend/photo/controller/PhotoController.java
@@ -2,6 +2,11 @@ package com.picscore.backend.photo.controller;
 
 import com.picscore.backend.photo.service.PhotoDTO;
 import com.picscore.backend.photo.service.PhotoService;
+import com.picscore.backend.user.jwt.JWTFilter;
+import com.picscore.backend.user.jwt.JWTUtil;
+import com.picscore.backend.user.service.OAuthService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,18 +20,24 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1")
+@RequiredArgsConstructor
 public class PhotoController {
 
     private final PhotoService photoService;
-
-    @Autowired
-    public PhotoController(PhotoService photoService) {
-        this.photoService = photoService;
-    }
-
+    private final OAuthService oAuthService;
     @GetMapping("user/photo/{userId}")
     public ResponseEntity<Map<String, Object>> getPhotosByUserId(@PathVariable Long userId) {
-        System.out.println("userId111111 = " + userId);
+        List<PhotoDTO> photos = photoService.getPhotosByUserId(userId);
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", "사진 조회 완료");
+        response.put("photos", photos);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("user/photo/me")
+    public ResponseEntity<Map<String, Object>> getMyPhotos(HttpServletRequest request) {
+        // 토큰에서 사용자 정보 추출
+        Long userId = oAuthService.findIdByNickName(request);
         List<PhotoDTO> photos = photoService.getPhotosByUserId(userId);
         Map<String, Object> response = new HashMap<>();
         response.put("message", "사진 조회 완료");
--- backend/src/main/java/com/picscore/backend/photo/service/PhotoService.java
+++ backend/src/main/java/com/picscore/backend/photo/service/PhotoService.java
@@ -2,6 +2,7 @@ package com.picscore.backend.photo.service;
 
 import com.picscore.backend.photo.entity.Photo;
 import com.picscore.backend.photo.repository.PhotoRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,24 +10,19 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class PhotoService {
     private final PhotoRepository photoRepository;
 
-    @Autowired
-    public PhotoService(PhotoRepository photoRepository) {
-        this.photoRepository = photoRepository;
-    }
-
     public List<PhotoDTO> getPhotosByUserId(Long userId) {
-        System.out.println("userId22222 = " + userId);
         List<Photo> photos = photoRepository.findPhotosByUserId(userId);
-        System.out.println("photos = " + photos);
         List<PhotoDTO> photoDTOs = photos.stream()
                 .map(photo -> new PhotoDTO(photo.getId(), photo.getImageUrl()))
                 .collect(Collectors.toList());
-        System.out.println("photoDTOs = " + photoDTOs);
         return photoDTOs;
     }
+
+
 }
 
 
