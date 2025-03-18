e33cccd1a8c7132e5e9e002b07a86c949f77841a
kub938 <kub938@naver.com>
2025-03-18 13:05:29 +0900

    feat: 공통 api 구현

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -14,9 +14,26 @@ api.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response) {
-      console.error("서버 응답 에러:", error.response.status);
+      const errorStatus = error.response.status;
+      console.error("서버 응답 에러:", errorStatus);
 
-      if (error.response.status === 401) {
+      switch (errorStatus) {
+        case 401:
+          console.error(`${errorStatus} Unauthorized: 인증 오류`);
+          break;
+        case 403:
+          console.error(`${errorStatus} Forbidden: 권한 오류`);
+          break;
+        case 404:
+          console.error(
+            `${errorStatus} Not Found: 요청한 리소스가 서버에 없음`
+          );
+          break;
+        case 422:
+          console.error(
+            `${errorStatus} Unprocessable Entity: 요청은 유효하나 처리 실패`
+          );
+          break;
       }
     } else if (error.request) {
       console.error("네트워크 에러:", error.request);
