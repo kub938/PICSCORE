6c6c3b4048fa1beb8ef6cc81f0f09b3d800f9a41
김재혁 <rublin322@gmail.com>
2025-03-20 16:15:23 +0900

    test: ci/cd test

--- backend/src/main/java/com/picscore/backend/user/handler/CustomSuccessHandler.java
+++ backend/src/main/java/com/picscore/backend/user/handler/CustomSuccessHandler.java
@@ -35,6 +35,7 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
      */
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        System.out.println("과연2");
 
         // OAuth2User 정보 가져오기
         CustomOAuth2User customUserDetails = (CustomOAuth2User) authentication.getPrincipal();
