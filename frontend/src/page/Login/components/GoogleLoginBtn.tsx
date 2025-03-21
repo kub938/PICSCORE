a693dc4cd39cee165c7c836bee1d47f168917c39
kub938 <kub938@naver.com>
2025-03-21 12:46:13 +0900

    feat: 환경변수 적용

--- frontend/src/page/Login/components/GoogleLoginBtn.tsx
+++ frontend/src/page/Login/components/GoogleLoginBtn.tsx
@@ -2,7 +2,7 @@ import googleLoginBtn from "../../../assets/Login/google-login-btn.svg";
 
 function GoogleLoginBtn() {
   const locateGoogleLogin = () => {
-    window.location.href = "http://localhost:8080/api/v1/user";
+    window.location.href = `${import.meta.env.VITE_API_URL}/api/v1/user`;
   };
   return (
     <button onClick={locateGoogleLogin}>
