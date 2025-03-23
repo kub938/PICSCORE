58939080049ddee2d015dc3991e9de2c780ae15f
kub938 <kub938@naver.com>
2025-03-23 22:04:40 +0900

    feat: 로그인 상태관리 재구현

--- frontend/src/page/Login/components/GoogleLoginBtn.tsx
+++ frontend/src/page/Login/components/GoogleLoginBtn.tsx
@@ -3,19 +3,6 @@ import googleLoginBtn from "../../../assets/Login/google-login-btn.svg";
 import { useAuthStore } from "../../../store/authStore";
 
 function GoogleLoginBtn() {
-  const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
-  const login = useAuthStore((state) => state.login);
-
-  useEffect(() => {
-    const params = new URLSearchParams(window.location.search);
-    const loginSuccess = params.get("loginSuccess");
-    console.log(loginSuccess);
-    if (loginSuccess) {
-      login();
-      console.log(isLoggedIn);
-    }
-  });
-
   const locateGoogleLogin = () => {
     window.location.href = `https://j12b104.p.ssafy.io/api/v1/user`;
   };
