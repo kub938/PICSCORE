bdca50294dd8a9fbdb73195fc11979cd97fdcd41
kub938 <kub938@naver.com>
2025-03-24 16:49:45 +0900

    feat: 로그인 접근 제한 구현

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -14,6 +14,7 @@ import TimeAttack from "../page/Timeattack/Timeattack";
 import TimeAttackResult from "../page/Timeattack/TimeAttackResult";
 import ImageUpload from "../page/ImageEval/ImageUpload";
 import ImageEvalResult from "../page/ImageEval/ImageEvalResult";
+import PrivateRouter from "./PrivateRouter";
 
 const router = createBrowserRouter([
   {
@@ -22,64 +23,69 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       {
-        index: true, // 부모 요소의 path와 일치하면 일로 보냄
-        element: <Home />,
-      },
-      {
-        path: "/test",
-        element: <Test />,
-      },
-      {
-        path: "/time-attack",
-        element: <TimeAttack />,
-      },
-      {
-        path: "/time-attack/result",
-        element: <TimeAttackResult />,
-      },
-      {
-        path: "/ranking",
-        element: <RankingPage />,
-      },
-      {
-        path: "/mypage",
-        element: <MyPage />,
-      },
-      {
-        path: "/user/:userId",
-        element: <UserDetailPage />,
-      },
-      {
-        path: "/change-info",
-        element: <ChangeInfoPage />,
-      },
-      {
-        path: "/archieve",
-        element: <ArchievePage />,
-      },
-      {
-        path: "/image-upload",
-        element: <ImageUpload />,
-      },
-      {
-        path: "/ranking",
-        element: <RankingPage />,
-      },
-      {
-        path: "/board",
-        element: <Board />,
-      },
-      {
-        path: "/contest",
-        element: <Contest />,
-      },
-      {
         path: "/login",
         element: <Login />,
       },
       {
-        path: "/image-result",
-        element: <ImageEvalResult />,
+        element: <PrivateRouter />,
+        children: [
+          {
+            index: true, // 부모 요소의 path와 일치하면 일로 보냄
+            element: <Home />,
+          },
+          {
+            path: "/time-attack",
+            element: <TimeAttack />,
+          },
+          {
+            path: "/time-attack/result",
+            element: <TimeAttackResult />,
+          },
+          {
+            path: "/ranking",
+            element: <RankingPage />,
+          },
+          {
+            path: "/mypage",
+            element: <MyPage />,
+          },
+          {
+            path: "/user/:userId",
+            element: <UserDetailPage />,
+          },
+          {
+            path: "/change-info",
+            element: <ChangeInfoPage />,
+          },
+          {
+            path: "/archieve",
+            element: <ArchievePage />,
+          },
+          {
+            path: "/image-upload",
+            element: <ImageUpload />,
+          },
+          {
+            path: "/ranking",
+            element: <RankingPage />,
+          },
+          {
+            path: "/board",
+            element: <Board />,
+          },
+          {
+            path: "/contest",
+            element: <Contest />,
+          },
+          {
+            path: "/login",
+            element: <Login />,
+          },
+          {
+            path: "/image-result",
+            element: <ImageEvalResult />,
+          },
+        ],
       },
     ],
   },
