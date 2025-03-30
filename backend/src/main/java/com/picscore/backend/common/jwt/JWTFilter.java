2d256caaaf02d780a5e6a6c04fe76eeee9d06f26
김경환 <k2sc2000@gmail.com>
2025-03-30 09:30:03 +0900

    hotfix: healthycheck

--- backend/src/main/java/com/picscore/backend/common/jwt/JWTFilter.java
+++ backend/src/main/java/com/picscore/backend/common/jwt/JWTFilter.java
@@ -55,15 +55,15 @@ public class JWTFilter extends OncePerRequestFilter {
         try {
             // 쿠키에서 액세스 토큰 추출
             String accessToken = null;
-//            Cookie[] cookies = request.getCookies();
-//            if (cookies != null) {
-//                for (Cookie cookie : cookies) {
-//                    if (cookie.getName().equals("access")) {
-//                        accessToken = cookie.getValue();
-//                        break;
-//                    }
-//                }
-//            }
+            Cookie[] cookies = request.getCookies();
+            if (cookies != null) {
+                for (Cookie cookie : cookies) {
+                    if (cookie.getName().equals("access")) {
+                        accessToken = cookie.getValue();
+                        break;
+                    }
+                }
+            }
 
             // 개발 환경 임시 방편
             String authorizationHeader = request.getHeader("Authorization");
